934880abf93afd418103456ed7c6d7d4ec4597da
totomo <bum4496@naver.com>
2020-09-05 23:47:16 +0900

    캐릭터 이동 변경
    - 좀더 부드럽게 이동
    - 점프 모션 추가
    - 중력 적용

--- Assets/Script/ThirdPersonMovement.cs
+++ Assets/Script/ThirdPersonMovement.cs
@@ -9,6 +9,17 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speed = 6f;
     public float runSpeedRate = 1.5f;
     public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public bool isGrounded;
+    Vector3 velocity;
 
     Animator animator;
     void Awake() {
@@ -16,33 +27,47 @@ public class ThirdPersonMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         bool hasHorizontalInput = !Mathf.Approximately(horizontal,0f);
+
         bool hasVeritcalInput = !Mathf.Approximately(vertical,0f);
 
         bool isWalk = hasHorizontalInput || hasVeritcalInput;
         bool isRun = Input.GetButton("Run") && isWalk;
 
-
         animator.SetBool("isWalk",isWalk);
         animator.SetBool("isRun",isRun);
     
-        Debug.Log(isRun);
-
-
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         // if is pressed either horizontal and vertical, diagnoal(대각선) is calculated root 2 because x and z is 1
         // so diagnoal's speed is faster than others
 
-
         if(direction.magnitude >= 0.1f){
-            transform.position += direction * speed * (isRun ? runSpeedRate : 1f) * Time.deltaTime;
 
-            transform.LookAt(transform.position + direction);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
+            controller.Move(direction * speed * (isRun ? runSpeedRate : 1f) * Time.deltaTime);
         }
+
+        bool isJump = Input.GetButtonDown("Jump");
+        animator.SetBool("isJump",isJump);
+        if(isJump && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity);
     }
 }
