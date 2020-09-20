1470cb1818f458e8bbcae42a22e343863e401e5b
totomo <bum4496@naver.com>
2020-09-20 20:01:19 +0900

    외계인 추가

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -13,42 +13,29 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothVelocity;
     public float gravity = -9.81f;
 
-    public float jumpHeight = 3f;
+    // public float jumpHeight = 3f;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+    // public Transform groundCheck;
+    // public float groundDistance = 0.4f;
+    // public LayerMask groundMask;
 
-    public bool isGrounded;
-    Vector3 velocity;
-
-    Animator animator;
-    void Awake() {
-        animator = GetComponentInChildren<Animator>();    
-    }
+    // public bool isGrounded;
+    // Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0){
-            velocity.y = -2f;
-        }
+        // if(isGrounded && velocity.y < 0){
+        //     velocity.y = -2f;
+        // }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal,0f);
-
-        bool hasVeritcalInput = !Mathf.Approximately(vertical,0f);
-
-        bool isWalk = hasHorizontalInput || hasVeritcalInput;
-        bool isRun = Input.GetButton("Run") && isWalk;
-
-        animator.SetBool("isWalk",isWalk);
-        animator.SetBool("isRun",isRun);
     
+        bool isRun = Input.GetButton("Run");
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         // if is pressed either horizontal and vertical, diagnoal(대각선) is calculated root 2 because x and z is 1
         // so diagnoal's speed is faster than others
@@ -62,14 +49,14 @@ public class ThirdPersonMovement : MonoBehaviour
             controller.Move(direction * speed * (isRun ? runSpeedRate : 1f) * Time.deltaTime);
         }
 
-        bool isJump = Input.GetButtonDown("Jump");
-        animator.SetBool("isJump",isJump);
+        // bool isJump = Input.GetButtonDown("Jump");
+        // animator.SetBool("isJump",isJump);
 
-        if(isJump && isGrounded){
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        // if(isJump && isGrounded){
+        //     velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        // }
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity);
+        // velocity.y += gravity * Time.deltaTime;
+        // controller.Move(velocity);
     }
 }
