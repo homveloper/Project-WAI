8666dc19a68e6f2ba8e7d738f2c2b3a8d2b7adf1
fsnyd <fsndudehd@naver.com>
2020-09-22 17:50:34 +0900

    뭔가많이추가됨

---
+++ Assets/Scripts/ResponeItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponeItem : MonoBehaviour
+{
+    public GameObject box;
+    int itemSize = 24;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateItem()
+    {
+        int cnt= 0;
+        int[] a = new int[10];
+        Transform[] points = GameObject.Find("RandomBoxRespone").GetComponentsInChildren<Transform>();
+        while(true)
+        {
+            int tmp =  Random.Range(1 ,itemSize );
+            a[cnt] = tmp;
+            cnt++;
+            if(cnt == 10)
+                break;    
+        }
+
+        for(int i = 0; i<cnt; i++)
+        {
+        Instantiate(box, points[a[i]].position , Quaternion.identity);
+        }
+    }
+}
