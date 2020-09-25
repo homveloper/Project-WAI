6c3e6ddf1b3ab4d7aed0f3152dbe939219fadad0
totomo <bum4496@naver.com>
2020-09-25 21:34:26 +0900

    캐릭터 이동 및 애니메이션 수정

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -33,8 +33,9 @@ public class ThirdPersonMovement : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        if (!photonView.IsMine)
-            return;
+        if(PhotonNetwork.IsConnected)
+            if (!photonView.IsMine)
+                return;
 
         // isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
