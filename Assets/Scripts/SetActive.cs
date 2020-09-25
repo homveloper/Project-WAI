f00f2f7c7f6022b5a5d4f46ab6bfbb7fd7a4e685
fsnyd <fsndudehd@naver.com>
2020-09-25 16:42:39 +0900

    dd

---
+++ Assets/Scripts/SetActive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour
+{
+    public GameObject info;
+    void Start()
+    {
+        info.SetActive(false);
+    }
+    // Start is called before the first frame update
+     private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + "감지 시작!");
+        info.SetActive(true);
+    }
+
+
+    // Collider 컴포넌트의 is Trigger가 true인 상태로 충돌중일 때
+
+    private void OnTriggerStay(Collider other)
+
+    {
+        Debug.Log(other.name + "감지 중!");
+        info.SetActive(true);
+
+    }
+
+
+
+    // Collider 컴포넌트의 is Trigger가 true인 상태로 충돌이 끝났을 때
+
+    private void OnTriggerExit(Collider other)
+
+    {
+        Debug.Log(other.name + "감지 끝!");
+        info.SetActive(false);
+    }
+}
