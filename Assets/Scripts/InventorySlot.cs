e3589f711f1f4e23a9a759acde38189d0250625c
totomo <bum4496@naver.com>
2020-10-05 23:46:49 +0900

    ㅇㅇ

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+
+    Item item;
+
+    public void AddItem(Item item){
+        this.item = item;
+        icon.sprite = item.icon;
+        icon.enabled = true; 
+    }
+
+    public void ClearSlot(){
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+}
