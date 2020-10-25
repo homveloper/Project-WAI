766b59698370c3cdfc2995e3bb4eb5bd89103973
totomo <bum4496@naver.com>
2020-10-25 21:11:00 +0900

    우주선추가

---
+++ Assets/Materials/Enviroment/TempleWall.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TempleWall", menuName = "Project-WAI/TempleWall", order = 0)]
+public class TempleWall : ScriptableObject {
+
+    public List<Mesh> bricks;
+    
+}
