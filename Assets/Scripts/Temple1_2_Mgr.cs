8c563ce0a6ed96033331fde931d9909778d0c8a1
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-10-26 20:04:44 +0900

    ddd
    dddd

---
+++ Assets/Scripts/Temple1_2_Mgr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple1_2_Mgr : MonoBehaviour
+{
+    public int cnt = 6;
+    public GameObject Goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cnt == 0)
+        {
+            Goal.SetActive(false);
+            cnt = 6;
+        }
+    }
+}
---
+++ Assets/Scripts/Temple1_2_Switch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temple1_2_Switch : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public GameObject swichFlash;
+     public GameObject mgr;
+
+     int tmp = 1;
+    void Start()
+    {
+        swichFlash.SetActive(false);
+    }
+    // Start is called before the first frame update
+     private void OnTriggerEnter(Collider other)
+    {
+        swichFlash.SetActive(true);
+        if(tmp == 1)
+        {
+            mgr.GetComponent<Temple1_2_Mgr>().cnt--;
+            tmp--;
+        }
+    }
+
+    // Collider 컴포넌트의 is Trigger가 true인 상태로 충돌중일 때
+
+    private void OnTriggerStay(Collider other)
+    {
+       
+    }
+}
