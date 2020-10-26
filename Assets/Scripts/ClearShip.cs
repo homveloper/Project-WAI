031aea1bd7b2a66b25b5b4ef6a510ac5aed2ba87
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-10-27 01:49:03 +0900

    dd
    ddd

---
+++ Assets/Scripts/ClearShip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearShip : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.tag == "Player")
+            GameManager.GetInstance().SetFinish(true);
+    }
+}
