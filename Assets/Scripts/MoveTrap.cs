c37ce5ecdef1f25d9e71d3a8bea78742a0a40238
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-11-02 16:57:03 +0900

    ddd
    ddddd

---
+++ Assets/Scripts/MoveTrap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrap : MonoBehaviour
+{
+    public float start;
+    public Material r1;
+    public Material r2;
+
+    float term = 30;
+    float moveTime = 3;
+
+    void Start()
+    {
+        StartCoroutine(this.moveTrap());
+        
+    }
+
+    IEnumerator moveTrap()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(start);
+           
+            gameObject.
+
+            yield return new WaitForSeconds(moveTime);
+
+           
+
+            yield return new WaitForSeconds(term);
+        }
+    }
+}
---
+++ Assets/Scripts/TrapTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject center;
+    void OnTriggerEnter(Collider other)
+    {
+       other.transform.position = center.transform.position;
+    }
+}
