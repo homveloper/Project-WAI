b14424fab240efa2a215be519dc2fbe85296d327
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-11-04 19:59:52 +0900

    dddd
    dddddd

--- Assets/Scripts/IceMove.cs
+++ Assets/Scripts/IceMove.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class IceMove : MonoBehaviour
 {
     // Start is called before the first frame updatepublic float speed = 5f;
-    public float speed = 5f;
-    private Rigidbody characterRigidbody;
+    public float speed;
+    public Rigidbody characterRigidbody;
  
-    void Start(){
-        characterRigidbody = GetComponent<Rigidbody>();
+    void Start()
+    {
+        //characterRigidbody = GetComponent<Rigidbody>();
     }
  
-    void Update(){
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            characterRigidbody.AddForce(-speed, 0, 0);
+    void FixedUpdate()
+    {
+        // if (Input.GetKey(KeyCode.LeftArrow)){
+        //     characterRigidbody.AddForce(-speed, 0, 0);
+        // }
+        // else if (Input.GetKey(KeyCode.RightArrow)){
+        //    characterRigidbody.AddForce(speed, 0, 0);
+        // }
+        // else if (Input.GetKey(KeyCode.UpArrow)){
+        //     characterRigidbody.AddForce(0, 0, speed);
+        // }
+        // else if (Input.GetKey(KeyCode.DownArrow)){
+        //     characterRigidbody.AddForce(0, 0, -speed);
+        // }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            characterRigidbody.velocity = new Vector3(-speed, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.RightArrow)){
-           characterRigidbody.AddForce(speed, 0, 0);
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+           characterRigidbody.velocity = new Vector3(speed, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.UpArrow)){
-            characterRigidbody.AddForce(0, 0, speed);
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            characterRigidbody.velocity = new Vector3(0, 0, speed);
         }
-        else if (Input.GetKey(KeyCode.DownArrow)){
-            characterRigidbody.AddForce(0, 0, -speed);
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            characterRigidbody.velocity = new Vector3(0, 0, -speed);
         }
     }
 }
