78b33d659d8c4a75944e36e5d4d38a9753289e70
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-11-12 20:26:59 +0900

    템플스위치스톤실험

--- Assets/Scripts/SetSwitch.cs
+++ Assets/Scripts/SetSwitch.cs
@@ -1,42 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class SetSwitch : MonoBehaviour
+public class SetSwitch : MonoBehaviourPun
 {
     // Start is called before the first frame update
     public GameObject swichFlash;
     public GameObject moveWell;
     public GameObject hiddenGate;
     public AudioSource wallSound;
+    public Transform temple;
+    public GameObject info;
+    Vector3 tmp;
+
+    int cnt = 1;
     void Start()
     {
         swichFlash.SetActive(false);
+        tmp = info.transform.localScale;
+        info.transform.localScale = new Vector3(0,0,0);
     }
     // Start is called before the first frame update
      private void OnTriggerEnter(Collider other)
     {
         /*if (other.GetComponent<PhotonView>().IsMine == false)
             return;*/
-
-        // Debug.Log(other.name + "감지 시작!");
-        swichFlash.SetActive(true);
-        if(moveWell == null)
+        if (other.GetComponent<PhotonView>() == null || other.GetComponent<PhotonView>().IsMine == false)
             return;
-        else
+        if(cnt == 1 || swichFlash.active == false)
         {
-            moveWell.SetActive(false);
-            wallSound.Play();
+            info.transform.localScale = tmp;
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cnt--;
+            swichFlash.SetActive(true);
+
+            if(moveWell == null)
+                return;
+            else
+            {
+                moveWell.SetActive(false);
+                wallSound.Play();
+            }
             
-        if(hiddenGate != null)
-            hiddenGate.SetActive(true);
+            if(hiddenGate != null)
+                hiddenGate.SetActive(true);
+        }
+        // Debug.Log(other.name + "감지 시작!");
     }
 
     // Collider 컴포넌트의 is Trigger가 true인 상태로 충돌중일 때
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEixt(Collider other)
     {
-       
+       info.transform.localScale = new Vector3(0,0,0);
     }
 }
