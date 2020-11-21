13810d068a3cbb3321d24e39292231760f7a6a68
Herase <phoebe7075@gmail.com>
2020-11-21 14:00:30 +0900

    413431415
    werqeqwrq312

---
+++ Assets/Scripts/MiniGame_enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame_enemy : MonoBehaviour
+{
+    public enum Type
+    {
+        Demon,
+        Jelly,
+        Wolf,
+        Chest
+    };
+    public Type type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetInteger("EnemyType", (int)type);
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MiniGame_enemyEditer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class MiniGame_enemyEditer : MonoBehaviour
+{
+    public Sprite demonSprite;
+    public Sprite jellySprite;
+    public Sprite wolfSprite;
+    public Sprite chestSprite;
+
+    private MiniGame_enemy.Type previousType;
+
+    private void Update()
+    {
+        MiniGame_enemy enemy = GetComponent<MiniGame_enemy>();
+        MiniGame_enemy.Type type = enemy.type;
+
+        if (type != previousType)
+        {
+            previousType = type;
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+            switch (type)
+            {
+                case MiniGame_enemy.Type.Demon:
+                    sr.sprite = demonSprite;
+                    break;
+
+                case MiniGame_enemy.Type.Jelly:
+                    sr.sprite = jellySprite;
+                    break;
+
+                case MiniGame_enemy.Type.Wolf:
+                    sr.sprite = wolfSprite;
+                    break;
+
+                case MiniGame_enemy.Type.Chest:
+                    sr.sprite = chestSprite;
+                    break;
+            }
+        }
+    }
+
+}
