6e8bcf2706d61f8b4421399d9284812d968f4feb
Herase <phoebe7075@gmail.com>
2020-11-22 14:16:55 +0900

    먼데이거
    왜없어;;;

--- Assets/Scripts/MiniGame_player.cs
+++ Assets/Scripts/MiniGame_player.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class MiniGame_player : MonoBehaviour
 {
     public float moveSpeed = 4;
