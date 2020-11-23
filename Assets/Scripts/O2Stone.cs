4d2818c6fea713dfe878ace158626c97fab75200
fsnyd <52208468+fsnyd@users.noreply.github.com>
2020-11-23 16:04:25 +0900

    dddd
    dddddddd

---
+++ Assets/Scripts/O2Stone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O2Stone : MonoBehaviour
+{
+    public GameObject swichFlash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        swichFlash.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag != "HitBox")
+            return ;
+        
+        other.GetComponentInParent<Player>().statO2++;
+        swichFlash.SetActive(true);
+    }
+}
