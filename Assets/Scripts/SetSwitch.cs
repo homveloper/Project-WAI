8ebc0916775f419888c83ed2260a662dd798523e
Herase <phoebe7075@gmail.com>
2020-11-24 00:09:04 +0900

    아닌건 뺐습니다 ㅎㅎ;
    ㅎㅎ

--- Assets/Scripts/SetSwitch.cs
+++ Assets/Scripts/SetSwitch.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SetSwitch : MonoBehaviour
+using Photon.Pun;
+public class SetSwitch : MonoBehaviourPun
 {
     // Start is called before the first frame update
     public GameObject swichFlash;
-    public GameObject moveWell;
+    public GameObject moveWall;
     public GameObject hiddenGate;
     public AudioSource wallSound;
     
@@ -18,7 +18,10 @@ public class SetSwitch : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-
+        if (cnt == 1)
+        {
+            photonView.RPC("OnSetSwitch", RpcTarget.AllBuffered);
+        }
     }
     void OnTriggerStay(Collider other)
     {
@@ -26,22 +29,23 @@ public class SetSwitch : MonoBehaviour
             return ;
         
         other.GetComponentInParent<Player>().statO2++;
-        swichFlash.SetActive(true);
-
-        if(moveWell == null)
-            return;
-        else
-        {
-            moveWell.SetActive(false);
-            wallSound.Play();
-        }
-            
-        if(hiddenGate != null)
-            hiddenGate.SetActive(true);
-
     }
+
     void OnTriggerExit(Collider other)
     {
         
+    }
+
+    [PunRPC]
+    public void OnSetSwitch()
+    {
+        cnt--;
+        swichFlash.SetActive(true);
+
+        moveWall.SetActive(false);
+        wallSound.Play();
+
+        if (hiddenGate != null)
+            hiddenGate.SetActive(true);
     }
 }
